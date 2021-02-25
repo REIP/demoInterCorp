353c9ff02d8180f24a8d5d87acfeaf9252c45ee5
Pier Herbozo Rivera <pier.herbozo@sap.com>
2021-02-25 11:20:42 -0500

    merge

--- InterGroup/src/main/java/intercorp/com/dao/IClientDao.java
+++
@@ -1,7 +0,0 @@
-package intercorp.com.dao;
-
-import intercorp.com.domain.Client;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IClientDao extends CrudRepository<Client, Integer> {
-}
